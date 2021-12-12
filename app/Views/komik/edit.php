e204eb5ed9ef79e8ee7dbdc77b59ad55f53f81f1
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2021-12-12 10:01:05 +0700

    update data fix bug

--- app/Views/komik/edit.php
+++ app/Views/komik/edit.php
@@ -11,7 +11,7 @@
                 <div class="form-group row">
                     <label for="judul" class="col-sm-2 col-form-label">Judul</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control <?= ($validation->hasError('judul')) ? 'is-invalid' : ''; ?>" id="judul" name="judul" autofocus value="<?= $komik['judul']; ?>">
+                        <input type="text" class="form-control <?= ($validation->hasError('judul')) ? 'is-invalid' : ''; ?>" id="judul" name="judul" autofocus value="<?= (old('judul')) ? old('judul') : $komik['judul'] ?>">
                         <div class="invalid-feedback">
                             <?= $validation->getError('judul'); ?>
                         </div>
@@ -20,7 +20,7 @@
                 <div class="form-group row">
                     <label for="penulis" class="col-sm-2 col-form-label">Penulis</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control <?= ($validation->hasError('penulis')) ? 'is-invalid' : ''; ?>" id="penulis" name="penulis" value="<?= $komik['penulis']; ?>">
+                        <input type="text" class="form-control <?= ($validation->hasError('penulis')) ? 'is-invalid' : ''; ?>" id="penulis" name="penulis" value="<?= (old('penulis')) ? old('penulis') : $komik['penulis'] ?>">
                         <div class="invalid-feedback">
                             <?= $validation->getError('penulis'); ?>
                         </div>
@@ -29,7 +29,7 @@
                 <div class="form-group row">
                     <label for="penerbit" class="col-sm-2 col-form-label">Penerbit</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control <?= ($validation->hasError('penerbit')) ? 'is-invalid' : ''; ?>" id="penerbit" name="penerbit" value="<?= $komik['penerbit']; ?>">
+                        <input type="text" class="form-control <?= ($validation->hasError('penerbit')) ? 'is-invalid' : ''; ?>" id="penerbit" name="penerbit" value="<?= (old('penerbit')) ? old('penerbit') : $komik['penerbit'] ?>">
                         <div class="invalid-feedback">
                             <?= $validation->getError('penerbit'); ?>
                         </div>
@@ -38,7 +38,7 @@
                 <div class="form-group row">
                     <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="sampul" name="sampul" value="<?= $komik['sampul']; ?>">
+                        <input type="text" class="form-control" id="sampul" name="sampul" value="<?= (old('sampul')) ? old('sampul') : $komik['sampul'] ?>">
                     </div>
                 </div>
                 <div class="form-group row">
